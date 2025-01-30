d3ac60a9c6b6b0ff202a12f478c19b30e7d4617c
Artificial-720 <76878013+Artificial-720@users.noreply.github.com>
2025-01-30 12:44:07 -0600

    Added Stop command.

---
+++ velocity/src/main/java/me/artificial/autoserver/velocity/commands/StopCommand.java
@@ -0,0 +1,67 @@
+package me.artificial.autoserver.velocity.commands;
+
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.command.SimpleCommand;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import me.artificial.autoserver.velocity.AutoServer;
+import net.kyori.adventure.text.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+public class StopCommand implements SubCommand {
+    private final AutoServer plugin;
+
+    public StopCommand(AutoServer plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void execute(CommandSource source, String[] args) {
+        if (args.length != 2) {
+            source.sendMessage(Component.text().content("Usage /autoserver stop <serverName>"));
+            return;
+        }
+
+        String serverName = args[1];
+        Optional<RegisteredServer> optionalServer = plugin.getProxy().getServer(serverName);
+        if (optionalServer.isEmpty()) {
+            source.sendMessage(Component.text().content("Server \"" + serverName + "\" not found. Please check the server name and try again"));
+            return;
+        }
+        RegisteredServer server = optionalServer.get();
+        source.sendMessage(Component.text().content("Stopping server \"" + serverName + "\"... Please wait."));
+
+        plugin.getServerManager().stopServer(server).whenComplete((result, ex) -> {
+            if (ex != null) { // error occurred
+                plugin.getLogger().info("Error: {}", ex.getMessage());
+                source.sendMessage(Component.text().content(ex.getMessage()));
+            } else {
+                plugin.getLogger().info("Message: {}", result);
+                source.sendMessage(Component.text().content(result));
+            }
+        });
+    }
+
+    @Override
+    public boolean hasPermission(SimpleCommand.Invocation invocation) {
+        return invocation.source().hasPermission("autoserver.command.stop");
+    }
+
+    @Override
+    public List<String> suggest(SimpleCommand.Invocation invocation) {
+        String[] args = invocation.arguments();
+        if (args.length == 2) {
+            String part = args[1].toLowerCase();
+            return plugin.getProxy().getAllServers().stream()
+                    .map(s -> s.getServerInfo().getName())
+                    .filter(name -> name.toLowerCase().startsWith(part)).toList();
+        }
+        return List.of();
+    }
+
+    @Override
+    public String help() {
+        return "Run the stop sequence for a server";
+    }
+}
