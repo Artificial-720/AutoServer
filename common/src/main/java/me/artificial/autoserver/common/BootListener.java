53b8d353faf2c4b8d2c6d9daae3588e77aed5816
Artificial-720 <76878013+Artificial-720@users.noreply.github.com>
2025-02-16 12:59:41 -0600

    BootListener added local cli commands. Improved error handling for boot listener.

--- common/src/main/java/me/artificial/autoserver/common/BootListener.java
+++ common/src/main/java/me/artificial/autoserver/common/BootListener.java
@@ -4,79 +4,133 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class BootListener {
+    private final static int CLIENT_TIMEOUT = 5000;
+    private final static int SERVER_RETRIES = 10;
+    private final static int DELAY_BETWEEN_RETIRES = 5000; // 5 seconds
 
-    private final int port;
-    private ServerSocket serverSocket;
     private final ExecutorService clientHandlers = Executors.newCachedThreadPool();
-    private boolean running = true;
-
-    public BootListener(int port) {
-        this.port = port;
-    }
+    private Integer port = null;
+    private BackendConfig config = null;
+    private ServerSocket serverSocket = null;
+    private volatile boolean running = true;
 
     public static void main(String[] args) {
+        BootListener bootListener = new BootListener();
+        bootListener.start();
+    }
+
+    private void start() {
+        if (!initialize()) return;
+
+        if (!setupServerSocket()) return;
+
+        sendBannerMessage();
+
+        new Thread(this::startSocketServer).start();
+
+        // Start loop for user cli input
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        while (running) {
+            System.out.print("> ");
+            try {
+                String command = r.readLine();
+                processCliCommand(command);
+            } catch (IOException ignored) {
+            }
+        }
+
+        cleanup();
+    }
+
+    private boolean initialize() {
+        String path;
+        try {
+            path = new File(BootListener.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
+        } catch (URISyntaxException e) {
+            System.err.println("Unable to resolve the file path for the config directory.");
+            return false;
+        }
+        this.config = new BackendConfig(new File(path, BackendConfig.DIRECTORY_NAME_DEFAULT));
+        this.port = config.getInt("bootListener.port");
+        if (port == null) {
+            System.err.println("Missing required setting \"bootListener.port\". Please add and restart.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean setupServerSocket() {
+        int retries = SERVER_RETRIES;
+        while (running && retries-- > 0) {
+            try {
+                assert port != null;
+                serverSocket = new ServerSocket(port);
+                return true;
+            } catch (IOException e) {
+                System.out.println("Error starting server: " + e.getMessage());
+            }
+
+            System.out.println("Retrying to start server in " + (DELAY_BETWEEN_RETIRES / 1000) + " seconds...");
+            try {
+                Thread.sleep(DELAY_BETWEEN_RETIRES);
+            } catch (InterruptedException e) {
+                System.out.println("Retry delay interrupted: " + e.getMessage());
+                Thread.currentThread().interrupt(); // Keep the interrupt going
+            }
+        }
+        System.err.println("Failed to setup server socket.");
+        return false;
+    }
+
+    private void sendBannerMessage() {
         long pid = ProcessHandle.current().pid();
         System.out.println("================================================");
         System.out.println("Boot Listener started with PID: " + pid);
         System.out.println("Current date: " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
-        int port = Integer.parseInt(Config.getProperty("port"));
-
-        new BootListener(port).start();
-
-        // Sleep for 10 seconds after finishing for debug
-        try {
-            Thread.sleep(1000 * 10);
-        } catch (InterruptedException e) {
-            System.out.println("Interrupt received");
-            Thread.currentThread().interrupt();
-        }
+        System.out.println("Config path: " + config.getConfigPath());
+        System.out.println("Boot Listener listening on port: " + port);
+        System.out.println("================================================");
     }
 
-    private void start() {
-        int retries = 10;
-        int delay = 5000; // 5 seconds
-        int timeout = 5000;
-        while (running && retries-- > 0) {
-            try {
-                serverSocket = new ServerSocket(port);
-                System.out.println("Boot Listener start on port: " + port);
-                while (running) {
-                    try {
-                        Socket clientSocket = serverSocket.accept();
-                        clientSocket.setSoTimeout(timeout);
-                        System.out.println("Connection received from: " + clientSocket.getInetAddress());
-                        clientHandlers.submit(() -> handleClient(clientSocket));
-                    } catch (SocketException e) {
-                        if (running) {
-                            System.err.println("ServerSocket accept error: " + e.getMessage());
-                        } else {
-                            System.out.println("Server shutting down gracefully.");
-                        }
-                        break;
-                    } catch (IOException e) {
-                        System.err.println("Error accepting client connection: " + e.getMessage());
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println("Error starting server: " + e.getMessage());
-            } finally {
-                closeServerSocket();
-            }
+    private void cleanup() {
+        running = false;
 
-            if (running) {
-                System.out.println("Retrying to start server in " + (delay / 1000) + " seconds...");
-                try {
-                    Thread.sleep(delay);
-                } catch (InterruptedException e) {
-                    System.out.println("Retry delay interrupted: " + e.getMessage());
-                    Thread.currentThread().interrupt(); // Keep the interrupt going
+        System.out.println("Shutting down server listener...");
+        try {
+            if (serverSocket != null && !serverSocket.isClosed()) {
+                serverSocket.close();
+                System.out.println("Server socket closed.");
+            }
+        } catch (IOException e) {
+            System.err.println("Error closing server socket: " + e.getMessage());
+        }
+        // stop client threads
+        clientHandlers.shutdownNow();
+    }
+
+    private void startSocketServer() {
+        while (running) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                clientSocket.setSoTimeout(CLIENT_TIMEOUT);
+                System.out.println("Connection received from: " + clientSocket.getInetAddress());
+                clientHandlers.submit(() -> handleClient(clientSocket));
+            } catch (SocketException e) {
+                if (running) {
+                    System.err.println("ServerSocket accept error: " + e.getMessage());
+                } else {
+                    System.out.println("Server shutting down gracefully.");
                 }
+                break;
+            } catch (IOException e) {
+                System.err.println("Error accepting client connection: " + e.getMessage());
             }
         }
     }
@@ -87,7 +141,7 @@ public class BootListener {
             // Read the client's message
             String command = reader.readLine();
             System.out.println("Received command: " + command);
-            processCommand(command, writer);
+            processRemoteCommand(command, writer);
         } catch (IOException e) {
             System.err.println("Error handling client: " + e.getMessage());
         } finally {
@@ -100,14 +154,13 @@ public class BootListener {
         }
     }
 
-    private void processCommand(String command, PrintWriter writer) {
+    private void processRemoteCommand(String command, PrintWriter writer) {
         switch (command) {
             case "BOOT_SERVER":
                 respond(writer, "ACKNOWLEDGED: Boot command received. Starting backend server...");
                 if (startBackendServer()) {
                     respond(writer, "COMPLETED: Backend server started.");
                     running = false;
-                    closeServerSocket(); // Gracefully close the server socket
                 } else {
                     respond(writer, "FAILED: Failed to start backend server.");
                 }
@@ -115,7 +168,6 @@ public class BootListener {
             case "SHUTDOWN_BOOT_LISTENER":
                 respond(writer, "SUCCESS: Shutting down boot listener.");
                 running = false;
-                closeServerSocket();
                 break;
             default:
                 respond(writer, "ERROR: Invalid command.");
@@ -127,26 +179,51 @@ public class BootListener {
         writer.println(message);
     }
 
-    private boolean startBackendServer() {
-        String command = Config.getProperty("startCommand");
-        if (command == null) {
-            System.err.println("Error reading start command.");
-            return false;
+    private void processCliCommand(String command) {
+        switch (command) {
+            case "help":
+                System.out.println("Commands:");
+                System.out.println("  help   - this message");
+                System.out.println("  start  - start the backend server");
+                System.out.println("  stop   - stop the boot listener");
+                System.out.println("  reload - reload the config.yml file");
+                break;
+            case "reload":
+                config.reload();
+                break;
+            case "stop":
+                running = false;
+                break;
+            case "start":
+                System.out.println("Starting backend server...");
+                if (startBackendServer()) {
+                    System.out.println("Command ran successfully.");
+                    running = false;
+                } else {
+                    System.out.println("Failed to start backend server.");
+                }
+                break;
+            default:
+                System.out.println("Unknown command.");
         }
-        return CommandRunner.runCommand(command);
     }
 
-    private void closeServerSocket() {
-        System.out.println("Shutting down Boot listener...");
-        try {
-            if (serverSocket != null && !serverSocket.isClosed()) {
-                serverSocket.close();
-                System.out.println("Server socket closed.");
-            }
-        } catch (IOException e) {
-            System.err.println("Error closing server socket: " + e.getMessage());
+    private boolean startBackendServer() {
+        String command = config.getString("server.startCommand");
+        if (command == null) {
+            System.err.println("Error reading start command. Check config for required setting \"server.startCommand\".");
+            return false;
         }
-        // stop client threads
-        clientHandlers.shutdownNow();
+
+        String workingDirectory = config.getString("server.workingDirectory");
+        Boolean preserveQuotes = config.getBoolean("server.preserveQuotes");
+
+        try {
+            CommandRunner.runCommand(workingDirectory, command, preserveQuotes);
+        } catch (RuntimeException e) {
+            return false;
+        }
+
+        return true;
     }
 }
